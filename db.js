328aff72ff9f166bc3242e583b2eaf06901ffa9e
Agustin Vazquez <agustinvsexto196@gmail.com>
2022-08-06 14:35:10 -0300

    Bot terminado

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
-// use `prisma` in your application to read and write data in your DB
 
-export default prisma;
+module.exports = prisma
 
--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 // const bcrypt = require("bcrypt");
 const qrcode = require("qrcode-terminal");
-const { Client, LocalAuth, MessageAck } = require("whatsapp-web.js");
-// import prisma from "./db";
-const { PrismaClient } = require("@prisma/client");
-// require('dotenv').config()
-// var axios = require('axios');
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const prisma = require("./db")
 
-// const config = (country) =>{
-//   return {
-//     method: 'get',
-//     url: `https://v3.football.api-sports.io/teams?country=${country}`,
-//     headers: {
-//       'x-rapidapi-key': `${process.env.API_KEY}`,
-//       'x-rapidapi-host': 'v3.football.api-sports.io'
-//     },
-//   };
-// } 
-
-const prisma = new PrismaClient();
 
 const client = new Client({
   authStrategy: new LocalAuth(),
@@ -37,115 +22,8 @@ client.on("ready", () => {
   console.log("Client is ready!");
 });
 
-// client.on("message", async (msg) => {
-//   const chat = await client.getChatById(msg.from);
-//   const messages = await chat.fetchMessages();
-//   const lastMsg = messages[messages.length - 3];
-//   console.log(lastMsg);
-//   if (lastMsg === undefined)
-//     return msg.reply(
-//       "No hemos podido obtener la información, intentalo de nuevo más tarde"
-//     );
-//   if (msg.body.toLowerCase().includes("admin")) {
-//     return client.sendMessage(
-//       msg.from,
-//       "Inserte email y contraseña separados por un espacio"
-//     );
-//   }
-//   if (msg.body.includes("@")) {
-//     const emailPass = msg.body.split(" ");
-//     const email = emailPass[0];
-//     const pass = emailPass[1];
-//     const existAdmin = await prisma.admin.findUnique({
-//       where: {
-//         email,
-//       },
-//     });
-//     if (existAdmin) {
-//       // comparar contraseña hasheada y avisar que tiene permitido realizar acciones de admin y revelarlas
-//       const isAdmin = await bcrypt.compare(pass, existAdmin.password);
-//       if (isAdmin) {
-//         // mostras acciones disponibles
-//         return client.sendMessage(
-//           msg.from,
-//           "Escriba AGREGAR_CONCIERTO para agregar un concierto, BORRAR_CONCIERTO para borrar uno o INFO_CONCIERTO para obtener el stock del mismo"
-//         );
-//       }
-//     } else {
-//       // hashear contraseña y crear admin y mandar mensaje de usuario creado con exito
-//       const hashedPass = await bcrypt.hash(pass, 10);
-//       const newAdm = await prisma.admin.create({
-//         data: {
-//           email,
-//           password: hashedPass,
-//         },
-//       });
-//       if (newAdm) {
-//         return client.sendMessage(msg.from, "Admin creado con éxito");
-//       }
-//     }
-//   }
-//   if (msg.body === "AGREGAR_CONCIERTO") {
-//     return msg.reply(
-//       "Escriba nombre, precio y cantidad de lugares del concierto, separados por un espacio"
-//     );
-//   }
-//   if (msg.body === "BORRAR_CONCIERTO" || msg.body === "INFO_CONCIERTO") {
-//     return msg.reply("Escriba nombre del concierto");
-//   }
-//   if (lastMsg.body === "AGREGAR_CONCIERTO") {
-//     const data = msg.body.split(" ");
-//     const name = data[0];
-//     const price = Number(data[1]);
-//     const stock = Number(data[2]);
-//     const newConcert = await prisma.concert.create({
-//       data: {
-//         name,
-//         price,
-//         stock,
-//       },
-//     });
-//     if (newConcert) {
-//       return msg.reply("Concierto agregado con exito");
-//     }
-//   } else if (lastMsg.body === "BORRAR_CONCIERTO") {
-//     const deleteConcert = await prisma.concert.delete({
-//       where: {
-//         name: msg.body,
-//       },
-//     });
-//     if (deleteConcert) {
-//       return msg.reply("Concierto eliminado con exito");
-//     }
-//   } else if (lastMsg.body === "INFO_CONCIERTO") {
-//     const info = await prisma.concert.findUnique({
-//       where: {
-//         name: msg.body,
-//       },
-//     });
-//     if (info) {
-//       return msg.reply(`Entradas restantes: ${info.stock}`);
-//     }
-//   }
-// });
-
-// client.on("message", async (message)=>{
-//   if (message.body.includes("resultado")) {
-//     return message.reply("Escriba el país de la liga y el club separados por un -")
-//   }
-//   if (message.body.includes("-")) {
-//     const request = message.body.split("-")
-//     axios(config(request[0])).then(res => {
-//       console.log(res.data.response[0])
-      
-//     }).catch(err => {
-//       console.error(err)
-//     })
-//   }
-// })
-
 client.on("message", async (message) => {
-  if (message.body.includes("hola")) {
+  if (message.body.toLowerCase().includes("hola")) {
     return message.reply("Buenas! Somos la odontología Caras Felices, escriba el número que pertenezca a la acción requerida: 1 - Agendar una consulta | 2 - Revisar mi agenda")
   }
   if (message.body == "1") {
@@ -156,9 +34,7 @@ client.on("message", async (message) => {
   }
   if (message.body.includes(":")) {
     const data = message.body.split(" ")
-    console.log(data)
     const date = new Date(`2022-${data[0].length < 2 ? 0 : ""}${data[0]}-${data[1]}T${data[2]}`)
-    console.log(date)
     try {
       const allAgendas = await prisma.agenda.findMany({
         where: {
@@ -195,6 +71,3 @@ client.on("message", async (message) => {
     }
   }
 })
-
-
-// console.log(new Date("August 15, 2022 16:00:00"))
